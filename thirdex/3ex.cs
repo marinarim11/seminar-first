c129e9bbd41f2615c9a9b3a0d91779f25b513039
Марина Гречко <marinagrecko@MacBook-Air-Marina.local>
2023-06-24 22:43:32 +0300

    first

---
+++ thirdex/3ex.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if(num1 % 2 == 0) {
+    Console.WriteLine(" Число четное");
+}
+else {
+    Console.WriteLine(" Число нечетное");
+}
---
+++ twoex/2ex.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] arr = new int[3];
+Console.WriteLine("Введите число");
+arr[0] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+arr[1] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+arr[2] = Convert.ToInt32(Console.ReadLine());
+
+int count = 3;
+
+int max = arr[0];
+
+while(count > 0) {
+    if(max < arr[count - 1]) {
+        max = arr[count - 1];
+    }
+    count=count-1;
+}
+Console.WriteLine("Макcимальное число");
+Console.WriteLine(max);
